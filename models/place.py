d9ff496cfb53115c068db7009c2d8fc6d0602cc3
ALU-Kid <i.rutazinda@alustudent.com>
2024-09-28 18:39:23 +0200

    k

--- models/place.py
+++ models/place.py
@@ -29,8 +29,16 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
 
-    # Many-to-Many relationship with Amenity using the place_amenity table
-    amenities = relationship("Amenity", secondary=place_amenity, viewonly=False)
+    amenities = relationship('Amenity', secondary=place_amenity, viewonly=False)
+    
+    # Ensure the Amenity class has a corresponding relationship attribute
+    class Amenity(BaseModel, Base):
+        """ Amenity class """
+        
+        __tablename__ = 'amenities'
+        
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship('Place', secondary=place_amenity, viewonly=False)
 
     # For FileStorage
     @property
