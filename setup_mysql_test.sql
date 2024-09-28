cc533dcc9cf69b1643115c43fd99e34f8c1579ec
ICYEZAGATORE <i,icyezagat1@alustudent.com>
2024-09-28 10:30:05 +0200

    SQL testting

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,3 +1,21 @@
+-- Creating the database hbnb_test_db if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Creating the user 'hbnb_test'@'localhost' with password 'hbnb_test_pwd' if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Revoking any existing privileges (to ensure there are no conflicts)
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'hbnb_test'@'localhost';
+
+-- Granting ALL PRIVILEGES on hbnb_test_db to hbnb_test
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+
+-- Granting SELECT privilege on performance_schema to hbnb_test
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+
+-- Finalizing the privilege assignment
+FLUSH PRIVILEGES;
+
 import json
 
 class FileStorage:
