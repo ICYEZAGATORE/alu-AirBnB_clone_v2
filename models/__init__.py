ce9884096435dee86c34c065ff8f082bd166a08d
ALU-Kid <i.rutazinda@alustudent.com>
2024-09-28 14:48:21 +0200

    init

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,13 @@
-#!/usr/bin/python3
+# models/engine/file_storage.py
+
+class FileStorage:
+    def __init__(self):
+        # Initialization code here
+        pass
+
+    def reload(self):
+        # Reload method implementation
+        pass#!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
 
