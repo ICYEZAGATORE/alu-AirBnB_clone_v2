79109b60f3b5ce3146ae06b2cb37c4e4800e188a
ALU-Kid <i.rutazinda@alustudent.com>
2024-09-28 17:47:43 +0200

    hhbbj

--- models/city.py
+++ models/city.py
@@ -1,9 +1,16 @@
-from sqlalchemy import Column, String, ForeignKey
+#!/usr/bin/python3
+"""This module defines a class City"""
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 class City(BaseModel, Base):
-    """City model to store city information."""
+    """This class defines a city by various attributes"""
     
     __tablename__ = 'cities'
+
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+    # Relationship to Place with cascading delete
+    places = relationship("Place", backref="city", cascade="all, delete")
