7429a32b91535623520185e763481a3f8a0260f0
ALU-Kid <i.rutazinda@alustudent.com>
2024-09-28 18:15:54 +0200

    naaaaaaaaaaaaaaaaa

--- models/amenity.py
+++ models/amenity.py
@@ -4,13 +4,12 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
-
 class Amenity(BaseModel, Base):
     """Represents an Amenity for a place"""
     
     __tablename__ = 'amenities'
-    
+
     name = Column(String(128), nullable=False)
 
-    # Relationship with Place for the many-to-many relationship
+    # Relationship with Place through the place_amenity table
     place_amenities = relationship("Place", secondary="place_amenity", viewonly=False)
